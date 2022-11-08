3cc526f673e38e77635f6a10484712a8303ebffc
Sohag <sohaghossain7th@gmail.com>
2022-11-09 01:21:58 +0600

    Initial setup server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello from Life Advice!")
+})
+
+app.listen(port, () => {
+    console.log(`Life Advice app listening on port ${port}`)
+})
